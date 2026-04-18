ded36a0908ce5d8db3c933af69a4840ffaf47588
Cristian <cristiangaticaoficial@gmail.com>
2026-04-18 15:51:33 -0300

    fix: increase body limit

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ import fetch from "node-fetch";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 app.post("/api/chat", async (req, res) => {
   try {
